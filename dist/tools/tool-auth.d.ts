f128599dbdd8f693cc36a90826af75430cd895f9
MingShuo-S <251250124@nju.edu.cn>
2026-05-28 22:12:37 +0800

    perf: remove dist/ from git (clone slowdown), add to gitignore

--- dist/tools/tool-auth.d.ts
+++
@@ -1,7 +0,0 @@
-import { Template } from '../types.js';
-export declare class ToolAuth {
-    static checkSlotAccess(agentName: string, slotName: string, action: 'read' | 'write', template: Template, currentStageIndex: number): void;
-    static getReadableSlots(template: Template, currentStageIndex: number): string[];
-    static getWritableSlots(template: Template, currentStageIndex: number): string[];
-}
-//# sourceMappingURL=tool-auth.d.ts.map
--- dist/types.js
+++
@@ -1,3 +0,0 @@
-// src/types.ts - 核心数据类型定义
-export {};
-//# sourceMappingURL=types.js.map
