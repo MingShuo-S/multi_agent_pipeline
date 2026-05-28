18e2f79c6f17b179f185fd44481c39cf8671a264
MingShuo-S <251250124@nju.edu.cn>
2026-05-28 11:48:26 +0800

    尝试2

--- handler.js
+++ handler.js
@@ -1,1 +1,4 @@
-export default {};
+// OpenClaw runtime entry (marketplace/zip installs).
+// Re-export the compiled plugin definition so tools are registered.
+export { default } from './dist/index.js';
+export * from './dist/index.js';
--- index.js
+++ index.js
@@ -2,4 +2,5 @@
 // This file re-exports from dist/index.js so that relative module paths resolve correctly
 // when OpenClaw loads plugins from the root directory.
 
+export { default } from './dist/index.js';
 export * from './dist/index.js';
